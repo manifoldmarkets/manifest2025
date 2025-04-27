57a24dc9b914249f1f73a03e7185eb77de88cb8d
Austin Chen <akrolsmir@gmail.com>
2025-04-26 23:24:05 -0700

    Update Next and React

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,8 +17,8 @@ export const metadata: Metadata = {
   title: 'Manifest',
   description: 'A festival for forecasting, markets, AI, and novel ideas',
   icons: {
-    icon: '/manifold-logo.ico'
-  }
+    icon: '/manifold-logo.ico',
+  },
 }
 
 export default function RootLayout({
